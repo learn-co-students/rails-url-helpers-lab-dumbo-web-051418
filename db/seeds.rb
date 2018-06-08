5c5e191990fc8aa62b81d3daa5a1ee0c734dd9ea
pablopawpaw <lpault29@gmail.com>
2018-06-08 16:06:50 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# mal = Student.create(first_name: "Malorie", last_name: "Casimir")
+# sam = Student.create(first_name: "Sammy", last_name: "Akharaz")
+# sam = Student.create(first_name: "Lawrence", last_name: "Hon")
+# Student.all.each {|student| student.active = true}
