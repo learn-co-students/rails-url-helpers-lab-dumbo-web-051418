df6e8bb0ad3ae157c96109534de47ae1afb9c7b4
github username <github email address>
2018-06-09 18:45:53 -0400

    commit 1

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,26 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    #@student = set_student
+    render :show
+  end
+
+  def activate
+    @student = set_student
+    if @student.active == true
+      @student.active = false
+    else
+      @student.active = true
+    end
+
+    @student.save
+    #redirect_to student_path(@student.id)
+    render :show
   end
 
   private
